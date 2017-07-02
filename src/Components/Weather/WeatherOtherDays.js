51bb6be9fbb9c3f7b91de2352bdcfe103d9308f6
Ashwin Chatterji <ashwinath@hotmail.com>
2017-07-02 12:58:00 +0800

    better mobile responsiveness

--- src/Components/Weather/WeatherOtherDays.js
+++ src/Components/Weather/WeatherOtherDays.js
@@ -9,17 +9,17 @@ class WeatherOtherDays extends Component {
       <div className="col-md-6 col-sm-12">
         {weatherInfo.map(dailyInfo => {
           return (
-            <div id="weather-other-info" className="col-sm-12">
-              <div className="col-sm-2">
+            <div id="weather-other-info" className="col-xs-12">
+              <div className="col-xs-2">
                 <i className={WeatherUtils.mapWeatherIdToIconCss(dailyInfo.WEATHER_ID)} />
               </div>
-              <div className="col-sm-6">
+              <div className="col-xs-6">
                 <p id="weather-other-date">{`${WeatherUtils.formatDay(dailyInfo.DATE)}, ${WeatherUtils.formatDate(dailyInfo.DATE)}`}</p>
               </div>
-              <div className="col-sm-2">
+              <div className="col-xs-2">
                 <p id="weather-other-max">{Math.round(dailyInfo.MAX)}°C</p>
               </div>
-              <div className="col-sm-2">
+              <div className="col-xs-2">
                 <p id="weather-other-min">{Math.round(dailyInfo.MIN)}°C</p>
               </div>
             </div>
